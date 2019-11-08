e62dc16b40ed6e16550a47f89f3dcab54fd758c6
Laurens-Post <contact@lphosting.nl>
2019-11-08 11:37:55 +0100

    Login screen

--- App.js
+++ App.js
@@ -4,7 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        <Input
+            id="email"
+            label="E-Mail"
+            keyboardType="email-address"
+            required
+            email
+            autoCapitalize="none"
+            errorMessage="Please enter a valid email address."
+            onInputChange={() => {}}
+            initialValue=""
+        />
+        <Input
+            id="password"
+            label="Password"
+            keyboardType="default"
+            secureTextEntry
+            required
+            minLength={5}
+            autoCapitalize="none"
+            errorMessage="Please enter a valid password."
+            onInputChange={() => {}}
+            initialValue=""
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="Login" color={Colors.primary} onPress={() => {}} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+              title="Register account"
+              color={Colors.accent}
+              onPress={() => {}}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
